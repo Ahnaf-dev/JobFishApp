4ab1b37942c9c02c2a947874fb8cbac419c9a88f
Ahnaf-dev <52428475+Ahnaf-dev@users.noreply.github.com>
2023-11-03 09:29:48 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require("./config/db");
 
 connectDB();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
